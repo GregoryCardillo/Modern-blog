cab1bbe4dce10f3b8caa591d2c86270df7e13b9f
Gregory Cardillo <gregoryincmail@gmail.com>
2025-12-01 23:47:16 +0100

    First commit

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+// Base Eloquent Model class
+use Illuminate\Database\Eloquent\Model;
+// Blueprint class to define table structures
+use Illuminate\Database\Schema\Blueprint;
+// Helper to create, modify, and manage database schemas
+use Illuminate\Support\Facades\Schema;
+
+
+class Post extends Model
+{
+    //
+}
