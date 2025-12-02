eb8774397ee10c6af9f9196c2fa783f36a33b4bc
Gregory Cardillo <gregoryincmail@gmail.com>
2025-12-02 10:35:48 +0100

    Add Post migration and model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,24 @@ namespace App\Models;
 
 // Base Eloquent Model class
 use Illuminate\Database\Eloquent\Model;
-// Blueprint class to define table structures
-use Illuminate\Database\Schema\Blueprint;
-// Helper to create, modify, and manage database schemas
-use Illuminate\Support\Facades\Schema;
 
+// This code define a model Post that map the "posts" table in the database and protect the coloumns from mass assignment. 
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        "title",
+        'slug',
+        'content',
+        'image',
+        'category',
+        'published_at',
+    ];
+
+// Cast published_at to a datetime object for easier date manipulations. 
+// Create an associative array where 'published_at is the key and 'datetime' is the value. 
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
 }
--- database/migrations/2025_12_01_222737_create_posts_table.php
+++ database/migrations/2025_12_01_222737_create_posts_table.php
@@ -1,7 +1,12 @@
 <?php
 
+// Migration class to create the posts table
 use Illuminate\Database\Migrations\Migration;
+
+// Blueprint class to define table structures
 use Illuminate\Database\Schema\Blueprint;
+
+// Helper to create, modify, and manage database schemas
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,6 +18,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->string('category')->default('News');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
